6d73882713e66bbdd261a519167addba89885709
vasudhamalla <33192943+vasudhamalla@users.noreply.github.com>
2025-08-27 13:06:14 -0500

    Time: 1 ms (19.48%) | Memory: 42.8 MB (20.78%) - LeetSync

--- 1164-missing-number-in-arithmetic-progression/missing-number-in-arithmetic-progression.java
+++ 1164-missing-number-in-arithmetic-progression/missing-number-in-arithmetic-progression.java
@@ -2,12 +2,11 @@ class Solution {
     public int missingNumber(int[] arr) {
         Arrays.sort(arr);
         int n = (arr[arr.length-1]-arr[0]) /(arr.length);
-        if(n==0) return arr[0];
         for(int i=0;i<arr.length-1;i++){
             if(n != arr[i+1]-arr[i]){
                 return arr[i]+n;
             }
         }
-        return n;
+        return arr[0];
     }
 }
