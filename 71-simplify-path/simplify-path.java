2d30c681196f075889b2ba743c10ad05bcb96b2f
vasudhamalla <33192943+vasudhamalla@users.noreply.github.com>
2025-08-31 23:02:50 -0500

    Time: 4 ms (91.51%) | Memory: 43.8 MB (36.79%) - LeetSync

--- 71-simplify-path/simplify-path.java
+++ 71-simplify-path/simplify-path.java
@@ -3,15 +3,17 @@ class Solution {
         String[] a = path.split("/");
         Stack<String> s1 = new Stack<>();
         Stack<String> s2 = new Stack<>();
-        if(a.length == 1) s1.push("/");
-        for(String s : a){
-            if(!s.equals("..") && !s .equals(".") && !s.equals("")){
+        if (a.length == 1)
+            s1.push("/");
+        for (String s : a) {
+            if (!s.equals("..") && !s.equals(".") && !s.equals("")) {
                 s1.push("/");
                 s1.push(s);
-            }
-            else if(s.equals("..")){
-                if(!s1.isEmpty()) s1.pop();
-                if(!s1.isEmpty()) s1.pop();
+            } else if (s.equals("..")) {
+                if (!s1.isEmpty())
+                    s1.pop();
+                if (!s1.isEmpty())
+                    s1.pop();
             }
 
         }
